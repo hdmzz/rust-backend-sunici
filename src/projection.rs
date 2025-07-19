ef840fc2ab3f38f6688cbfde6e9ad09ff995762e
hugo <hdamitzian@gmail.com>
2025-07-19 23:29:20 +0200

    on a un resultat, enorme donc ==> approche binaire + compression suite dans le prochain episode...

--- src/projection.rs
+++ src/projection.rs
@@ -1,9 +1,10 @@
 use crate::models::Bbox;
+use std::f64::consts::PI;
 
 /// Projette des coordonnées géographiques (lon, lat) dans un système de coordonnées
 /// de scène 3D local défini par une Bbox et une taille de côté.
 /// C'est l'équivalent de `HugoGeo.projectCoord`.
-pub fn project_coords(lon_lat: [f64; 2], bbox: Bbox, units_side: f64) -> [f64; 2] {
+pub fn project_coords(lon_lat: [f64; 2], bbox: &Bbox, units_side: f64) -> [f64; 2] {
     let lon: f64 = lon_lat[0];
     let lat: f64 = lon_lat[1];
 
@@ -17,4 +18,19 @@ pub fn project_coords(lon_lat: [f64; 2], bbox: Bbox, units_side: f64) -> [f64; 2
     let y: f64 = y_ratio * units_side - units_side / 2.0;
 
     [x, y]
+}
+
+
+pub fn spherical_mercator_ll(px: (f64, f64), zoom: u8, tile_size: f64) -> (f64, f64) {
+    const R2D: f64 = 180.0 / PI;
+
+    let size = tile_size * (2u32.pow(zoom as u32)) as f64;
+    let bc = size / 360.0;
+    let cc = size / (2.0 * PI);
+    let zc = size / 2.0;
+    let g = (px.1 - zc) / -cc;
+    let lon = (px.0 - zc) / bc;
+    let lat = R2D * (2.0 * (g.exp()).atan() - 0.5 * PI);
+
+    (lon, lat)
 }
--- src/rgb_model.rs
+++ src/rgb_model.rs
@@ -1,44 +1,34 @@
 use actix_web::{error::ErrorInternalServerError, Error};
-use crate::models::{ApiResponse, Bbox, TerrainRequest};
-use std::{collections::HashMap, env};
+use crate::{models::{Bbox, TerrainRequest}, projection::project_coords};
+use std::collections::{HashMap, HashSet};
 use futures::future::join_all;
+use bytes::{Bytes};
+use crate::projection::spherical_mercator_ll;
+
 const CONST_VERTICES: usize = 128;
 
 pub fn get_zoom_position_elevation(zp_covered: &[Vec<u32>]) -> Vec<Vec<u32>> {
-    // HashMap pour stocker les groupements. La clé est une chaîne de caractères représentant
-    // les coordonnées du grand-parent ("z,x,y").
     let mut elevations: HashMap<String, Vec<Vec<u32>>> = HashMap::new();
 
     for zoom_pos in zp_covered {
-        // S'assure que la tuile a bien 3 composantes [z, x, y].
         if zoom_pos.len() == 3 {
-            // Calcule les coordonnées du grand-parent.
-            // `checked_sub` est utilisé pour éviter une panique si z < 2.
             if let Some(z) = zoom_pos[0].checked_sub(2) {//verifie si le niveau de zoom n'est pas inferieur a 2 sinon return None
                 let grandparent_z = z;
-                // La division entière sur des entiers positifs en Rust est équivalente à Math.floor().
                 let grandparent_x = zoom_pos[1] / 4;
                 let grandparent_y = zoom_pos[2] / 4;
 
-                // Crée la clé pour le HashMap.
                 let key = format!("{},{},{}", grandparent_z, grandparent_x, grandparent_y);
 
-                // Utilise l'API `entry` pour insérer une nouvelle entrée ou ajouter à une existante.
-                // `or_default()` insère un `Vec` vide si la clé n'existe pas, puis `push` ajoute l'élément qui est une copie==> to_vec()
                 elevations.entry(key).or_default().push(zoom_pos.to_vec());
             }
         }
     }
 
-    // La fonction originale renvoie les clés uniques (les grands-parents).
-    // Nous extrayons les clés du HashMap, les divisons et les reconvertissons en nombres.
     elevations
         .keys()
         .map(|triplet_str| {
             triplet_str
                 .split(',')
-                // `filter_map` est utilisé pour parser chaque partie en u32.
-                // Si le parsing échoue, `ok()` renvoie `None` et l'élément est ignoré.
                 .filter_map(|num_str| num_str.parse::<u32>().ok())
                 .collect::<Vec<u32>>()
         })
@@ -46,15 +36,45 @@ pub fn get_zoom_position_elevation(zp_covered: &[Vec<u32>]) -> Vec<Vec<u32>> {
 }
 
 
-async fn fetch_data( url: &String ) -> Result<ApiResponse, Error> {
+async fn fetch_data( url: &String ) -> Result<Bytes, Error> {
     let response = reqwest::get(url)
         .await
         .map_err(ErrorInternalServerError)?;
-    let api_response = response
-        .json::<ApiResponse>()
-        .await
-        .map_err(ErrorInternalServerError)?;
-    Ok(api_response)
+    let image_bytes = response
+    .bytes()
+    .await
+    .map_err(ErrorInternalServerError);
+
+    Ok(image_bytes?)
+}
+
+pub fn get_sixteenth_pixel_ranges() -> Vec<[[usize; 2]; 2]> {
+    const COLS: usize = 512;
+    const ROWS: usize = 512;
+    const SCALE_FACTOR: usize = 4;
+    const SUB_TILE_COUNT: usize = SCALE_FACTOR * SCALE_FACTOR; // 16
+
+    let mut ranges = Vec::with_capacity(SUB_TILE_COUNT);
+
+    let row_step = ROWS / SCALE_FACTOR; // 128
+    let col_step = COLS / SCALE_FACTOR; // 128
+
+    for c in 0..SCALE_FACTOR {
+        for r in 0..SCALE_FACTOR {
+            let row_start = r * row_step;
+            let row_end = (r + 1) * row_step;
+
+            let col_start = c * col_step;
+            let col_end = (c + 1) * col_step;
+
+            ranges.push([
+                [row_start, row_end],
+                [col_start, col_end],
+            ]);
+        }
+    }
+
+    ranges
 }
 
 pub struct RgbModel {
@@ -72,8 +92,10 @@ impl RgbModel {
         //1 recuperer BBox
         let bbox: &Bbox = &params.bbox;//la reference est utiliser si on ne lutilise pas cela creer une copie
         //2 recuperer zoom position covered ==> les 81 tuiles .....
-        let zoom_position_covered: Vec<Vec<u32>> = params.zoom_position_covered.clone();
+        let zoom_position_covered: Vec<Vec<u32>> = params.zoom_position_covered
+        .clone();
         let zoom_position_elevation: Vec<Vec<u32>> = get_zoom_position_elevation(&zoom_position_covered);
+
         self.fetch(&zoom_position_covered, bbox).await;
 
         println!("grandparents uniques: {:?}", zoom_position_elevation);
@@ -82,7 +104,9 @@ impl RgbModel {
     pub async fn fetch( &mut self, zp_covered: &[Vec<u32>], bbox: &Bbox ) {
         let zoom_position_elevation = get_zoom_position_elevation(zp_covered);
 
-        let fetch_futures = zoom_position_elevation.iter().map(|zoom_position| {
+        let fetch_futures = zoom_position_elevation
+        .iter()
+        .map(|zoom_position| {
             let url =self.get_uri(&zoom_position).clone();
             println!("{:?}", url);
             async move {
@@ -96,18 +120,89 @@ impl RgbModel {
         .into_iter()
         .zip(zoom_position_elevation.iter())
         .map(|(tile_result, zoom_pos)| {
-            let tile = tile_result.expect("Échec de la re cupération de la tuile");
-            self.add_tile(&tile, zoom_pos, zp_covered, bbox)
+            let tile_bytes = tile_result.expect("Échec de la re cupération de la tuile");
+            self.add_tile(&tile_bytes, zoom_pos, zp_covered, bbox)
         })
         .flatten()
         .collect::<Vec<_>>();
+
+        println!("{:?}", new_data_segments);
     }
 
-    fn add_tile(&self, tile: &ApiResponse, zoom_pos: &Vec<u32>, zp_covered: &[Vec<u32>], bbox: &Bbox) -> Vec<i32> {
-        let mut ret = Vec::new();
+    fn add_tile(&self, tile_bytes: &Bytes, zoom_position_elevation: &Vec<u32>, zp_covered: &[Vec<u32>], bbox: &Bbox) -> Vec<(Vec<i32>, Vec<f64>, Vec<u32>)> {
+        let decoder = png::Decoder::new(tile_bytes.as_ref());
+        let mut reader = match decoder.read_info() {
+            Ok(r) => r,
+            Err(_) => return Vec::new()
+        };
 
-        ret.push(5);
-        ret
+        let mut buff= vec![0; reader.output_buffer_size()];
+
+        if reader.next_frame(&mut buff).is_err() {
+            return Vec::new();
+        }
+
+        let elevations: Vec<f64> = buff
+        .chunks_exact(4)
+        .map(|pixel| {
+            let r = pixel[0] as f64;
+            let g = pixel[1] as f64;
+            let b = pixel[2] as f64;
+
+            -10000.0 + ((r * 256.0 * 256.0 + g * 256.0 + b) * 0.1)
+        })
+        .collect();
+        //Le tableau elevation contient les données d'elevation en mètres, pas de coordonnées donc il s'agit mainteant d'associer les données aux coordonnées
+
+        let sixteenth = self.get_sixteenth_tiles_coords(zoom_position_elevation);
+        let zoom_position_covered_str_set: HashSet<String> = zp_covered.iter().map(|zp| {format!("{}/{}/{}", zp[0], zp[1], zp[2])}).collect();
+        let sixteenth_pixel_range = get_sixteenth_pixel_ranges();
+
+        let mut data_elevation:  Vec<(Vec<i32>, Vec<f64>, Vec<u32>)> = Vec::new();
+
+        for (index, zoom_pos_str) in sixteenth.iter().enumerate() {
+            if !zoom_position_covered_str_set.contains(zoom_pos_str) {
+                continue;
+            };
+
+            let zoom_pos : Vec<i32> = zoom_pos_str.split('/').map(|s| s.parse().expect("erreur")).collect();
+
+            let px_range = &sixteenth_pixel_range[index];
+            let mut elev = Vec::with_capacity(128 *128);
+            for r in px_range[0][0]..px_range[0][1] {
+                for c in px_range[1][0]..px_range[1][1] {
+                    elev.push(elevations[r * 512 + c]);
+                };
+            };
+            
+            let mut array = Vec::with_capacity(CONST_VERTICES * CONST_VERTICES * 3);
+            let mut data_index = 0;
+            
+            for r in 0..128 {
+                for c in 0..128 {
+                    let lon_lat_pixel = spherical_mercator_ll(
+                        ((zoom_pos[1] as usize * 128 + c) as f64,
+                        (zoom_pos[2] as usize * 128 + r) as f64),
+                        zoom_pos[0] as u8, 
+                        512.0
+                    );
+
+                    let [x, y] = project_coords(
+                        [lon_lat_pixel.0, lon_lat_pixel.1],
+                        bbox,
+                        10000.0
+                    );
+
+                    array.push(x);
+                    array.push(y);
+                    array.push(elevations[data_index] * 10000.0);
+                    data_index += 1;
+                }
+            }
+            data_elevation.push((zoom_pos, array, zoom_position_elevation.clone()));
+        };
+            
+        data_elevation
     }
 
     fn get_uri(&self, zoom_pos: &[u32]) -> String {
@@ -116,5 +211,23 @@ impl RgbModel {
         };
     
         format!( "https://api.mapbox.com/v4/mapbox.terrain-rgb/{}/{}/{}@2x.pngraw?access_token={}", z, x, y, self.mapbox_token)
+    }
+
+    fn get_sixteenth_tiles_coords(&self, zoom_position_elevation: &Vec<u32>) -> Vec<String> {
+        let parent_z = zoom_position_elevation[0];
+        let parent_x = zoom_position_elevation[1];
+        let parent_y = zoom_position_elevation[2];
+
+        let mut sixteenth: Vec<String> = Vec::with_capacity(16);
+        for col in 0..4 {
+            for row in 0..4 {
+                let child_z = parent_z + 2;
+                let child_x = parent_x * 4 + col;
+                let child_y = parent_y * 4 + row;
+
+                sixteenth.push(format!("{}/{}/{}", child_z, child_x, child_y));
+            };
+        };
+        sixteenth        
     }
 }
